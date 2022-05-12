76195afa4cc9aaabba41e54883993b0c476dd5c4
amonsour <pureleb@gmail.com>
2022-05-12 08:45:02 -0400

    Updates

--- projects/swimlane/ngx-charts/src/lib/line-chart/line-chart.component.ts
+++ projects/swimlane/ngx-charts/src/lib/line-chart/line-chart.component.ts
@@ -136,6 +136,7 @@ import { isPlatformServer } from '@angular/common';
                 [colors]="colors"
                 [data]="series"
                 [scaleType]="scaleType"
+                [showDataPointCircles]="showDataPointCircles"
                 [visibleValue]="hoveredVertical"
                 [activeEntries]="activeEntries"
                 [tooltipDisabled]="tooltipDisabled"
@@ -232,6 +233,7 @@ export class LineChartComponent extends BaseChartComponent {
   @Input() xScaleMax: number;
   @Input() yScaleMin: number;
   @Input() yScaleMax: number;
+  @Input() showDataPointCircles: boolean = false;
 
   @Output() activate: EventEmitter<any> = new EventEmitter();
   @Output() deactivate: EventEmitter<any> = new EventEmitter();
